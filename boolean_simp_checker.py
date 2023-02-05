a357ce81c141304f602653213d57b53feefe33b6
unknown <chall168421@gmail.com>
2023-02-05 16:58:03 +0700

    tidy code

--- boolean_simp_checker.py
+++ boolean_simp_checker.py
@@ -1,13 +1,17 @@
 
 INVALID = "invalid expression"
+RED = "\n\033[38;2;254;0;0m"
+GREEN = "\n\033[38;2;0;254;0m"
+WHITE = "\033[38;2;254;254;254m"
 
 def get_terms(exp):
-
+    '''Retrieve boolean terms from expression'''
     return sorted(list(set(char for char in exp if char.isalpha())))
 
+###################################################
 
 def eval_exp(exp, bits, terms):
-
+    '''Pythonise boolean expression and then evaluate with given input bits'''
     exp = exp.replace("!", " not ")
     exp = exp.replace("+", " or ")
     exp = exp.replace(".", " and ")
@@ -15,75 +19,65 @@ def eval_exp(exp, bits, terms):
     exp = exp.replace("OR", " or ")
     exp = exp.replace("NOT", " not ")
 
-
     bits = list(bits)
 
     if terms:
         while bits:
             exp = exp.replace(terms.pop(0), bits.pop(0))
 
-
-    exp = "".join("0" if char.isupper() else char for char in exp)
-    
+    exp = "".join("0" if char.isupper() else char for char in exp)    
     exp = exp.replace("1", "True").replace("0", "False")
-
-
-    
-    
         
-    try:
-     
-        return str(int(eval(exp)))
-    except Exception as e:
-        print(exp)
-        print(e)
-        return INVALID
+    try:     
+        return str(int(eval(exp))), ""
+    except Exception as e:       
+        return INVALID, exp+e
 
-
+###################################################
 
 def compute_truth_table(exp, terms):
-
-    headings = ""
-
-    
+    '''Compute / display truth table for expression and return results'''
+    headings = ""    
 
     for term in terms:
         headings += term.center(3) + "|"
 
-    exp_length = len(exp)
-    
+    exp_length = len(exp)    
     headings += " " + exp
-
     print(headings)
-
     n_terms = len(terms)
-
     results = []
-
         
     for i in range(2 ** n_terms):
         print(" ", end="")
-
         bits = bin(i)[2:].zfill(n_terms)
-        results.append(eval_exp(exp, bits, list(terms)))
+        res, error = eval_exp(exp, bits, list(terms))
+        results.append(res)
         print(" | ".join(str(b) for b in bits) + f" | {results[-1]}")
 
-        if results[-1] == INVALID:
+        if res == INVALID:
             break
+
+    print(RED+error)
+    print(WHITE+" ")
 
     return results
 
+###################################################
 
 def get_exp(n):
+    '''Get expression from user'''
     exp = input(f"Enter {n} expression:  ")
 
     ## TODO later - syntax validation
     return exp
 
+###################################################
 
 def check_simp():
-
-    print("""\033[38;2;254;254;254mWhen entering expressions
+    '''Compare two simplifications and inform user if match found'''
+    
+    print(f"""{WHITE}When entering expressions
 use UPPERCASE for terms only.
 You may use either GCSE-style logic notation OR boolean algebra notation.
 (use ! for NOT instead of an overline)
@@ -110,11 +104,12 @@ not(not a and not b) or (c and not d)
     res2 = compute_truth_table(exp2, terms)
 
     if res1 != res2:
-        print("\n\033[38;2;254;0;0m"+"X Your boolean simplification did not work - results do not match.\n")
+        print(RED+"X Your boolean simplification did not work - results do not match.\n")
     else:
-        print("\n\033[38;2;0;254;0m"+"✓ Your boolean simplification worked - results matched. 🎉")
+        print(GREEN+"✓ Your boolean simplification worked - results matched. 🎉")
         print()
 
+###################################################
 
 if __name__ == "__main__":
     while True:
